d92dc2c16f2fd7840ff6e4b881897f4074435db2
Mariam shatta <mshatta@student.cscc.edu>
2026-09-21 19:14:42 -0400

    Add UPC check digit function
    Added a function to calculate the UPC check digit based on the first eleven digits.

--- Lab8_Mariashatta18-1.py
+++ Lab8_Mariashatta18-1.py
@@ -6,3 +6,19 @@ the expected check digit and comparing it with the check digit entered by the us
 Starter Code: No starter code was used.
 Date: September 21, 2026
 """
+def find_UPC(first_eleven):
+    odd_sum = 0
+    even_sum = 0
+
+    for i in range(11):
+        digit = int(first_eleven[i])
+
+        if i % 2 == 0:
+            odd_sum += digit
+        else:
+            even_sum += digit
+
+    total = (odd_sum * 3) + even_sum
+    check_digit = (10 - (total % 10)) % 10
+
+    return check_digit
