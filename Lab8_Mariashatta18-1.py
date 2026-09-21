85620658b18edf5a02b308145dc67033c8de17a3
Mariam shatta <mshatta@student.cscc.edu>
2026-09-21 19:06:28 -0400

    Add program header and documentation
    This program checks the validity of a 12-digit UPC-A code.

---
+++ Lab8_Mariashatta18-1.py
@@ -0,0 +1,8 @@
+"""
+Program Name: UPC Validator
+Author: Maria Shatta
+Purpose: This program checks whether a 12-digit UPC-A code is valid by calculating
+the expected check digit and comparing it with the check digit entered by the user.
+Starter Code: No starter code was used.
+Date: September 21, 2026
+"""
